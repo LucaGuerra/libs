a4d213ca1013a16ff53c8de28468ef4c10da56c5
Luca Guerra <luca@guerra.sh>
2022-05-06 12:23:04 +0000

    skel for gvisor engine

---
+++ userspace/libscap/engine/gvisor/gvisor.cpp
@@ -0,0 +1,174 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
+namespace scap_gvisor {
+	class engine;
+}
+
+#define SCAP_HANDLE_T scap_gvisor::engine
+
+#include "scap.h"
+#include "gvisor.h"
+#include "scap-int.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/epoll.h>
+#include <sys/stat.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+static SCAP_HANDLE_T *gvisor_alloc_handle(scap_t* main_handle, char *lasterr_ptr)
+{
+	printf("gvisor_alloc_handle\n");
+	return NULL;
+	/*
+	return (scap_ctx *) new scap_gvisor::engine(lasterr_ptr);
+	*/
+}
+
+static int32_t gvisor_init(scap_t* main_handle, scap_open_args* open_args)
+{
+	printf("gvisor_init\n");
+	return SCAP_SUCCESS;
+	/*
+	const char *socket_path = (const char*)param;
+    scap_gvisor::engine *gvisor_ctx = (scap_gvisor::engine *)ctx;
+	return gvisor_ctx->open(socket_path);
+	*/
+}
+
+static void gvisor_free_handle(struct scap_engine_handle engine)
+{
+	printf("gvisor_free_handle\n");
+	/*
+	scap_gvisor::engine *gvisor_ctx = (scap_gvisor::engine *)ctx;
+    delete gvisor_ctx;
+	*/
+}
+
+static int32_t gvisor_start_capture(struct scap_engine_handle engine)
+{
+	printf("gvisor_start_capture\n");
+	return SCAP_SUCCESS;
+	/*
+    scap_gvisor::engine *gvisor_ctx = (scap_gvisor::engine *)ctx;
+	return gvisor_ctx->start_capture();
+	*/
+}
+
+static int32_t gvisor_close(struct scap_engine_handle engine)
+{
+	printf("gvisor_close\n");
+	// should we unlink the socket?
+
+	return SCAP_SUCCESS;
+}
+
+static int32_t gvisor_stop_capture(struct scap_engine_handle engine)
+{
+	printf("gvisor_stop_capture\n");
+
+	return SCAP_SUCCESS;
+	/*
+    scap_gvisor::engine *gvisor_ctx = (scap_gvisor::engine *)ctx;
+	return gvisor_ctx->stop_capture();
+	*/
+}
+
+int32_t gvisor_next(struct scap_engine_handle engine, scap_evt **pevent, uint16_t *pcpuid)
+{
+	printf("gvisor_next\n");
+
+	return SCAP_SUCCESS;
+	/*
+	struct scap_gvisor::engine *gvisor_ctx = (scap_gvisor::engine *)ctx;
+	return gvisor_ctx->next(pevent, pcpuid);
+	*/
+}
+
+/*
+int32_t getpid_global(scap_ctx* ctx, int64_t *pid)
+{
+	*pid = 0;
+	return SCAP_SUCCESS;
+}*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+const struct scap_vtable scap_nodriver_engine = {
+	.name = "nodriver",
+	.mode = SCAP_MODE_NODRIVER,
+
+	.alloc_handle = alloc_handle,
+	.init = NULL,
+	.free_handle = noop_free_handle,
+	.close = noop_close_engine,
+	.next = next,
+	.start_capture = noop_start_capture,
+	.stop_capture = noop_stop_capture,
+	.configure = noop_configure,
+	.get_stats = noop_get_stats,
+	.get_n_tracepoint_hit = noop_get_n_tracepoint_hit,
+	.get_n_devs = noop_get_n_devs,
+	.get_max_buf_used = noop_get_max_buf_used,
+};
+*/
+
+/*
+struct scap_vtable scap_udig_engine = {
+	.name = "udig",
+	.mode = SCAP_MODE_LIVE,
+
+	.match = match,
+	.alloc_handle = alloc_handle,
+	.init = init,
+	.free_handle = free_handle,
+	.close = close_engine,
+	.next = next,
+	.start_capture = start_capture,
+	.stop_capture = stop_capture,
+	.configure = configure,
+	.get_stats = get_stats,
+	.get_n_tracepoint_hit = get_n_tracepoint_hit,
+	.get_n_devs = get_n_devs,
+	.get_max_buf_used = get_max_buf_used,
+};
+*/
+
+extern const struct scap_vtable scap_gvisor_engine = {
+	.name = "gvisor",
+	.mode = SCAP_MODE_LIVE,
+
+	// .match ???
+	.alloc_handle = gvisor_alloc_handle,
+	.init = gvisor_init,
+	.free_handle = gvisor_free_handle,
+	.close = gvisor_close,
+	.next = gvisor_next,
+	.start_capture = gvisor_start_capture,
+	.stop_capture = gvisor_stop_capture,
+};
