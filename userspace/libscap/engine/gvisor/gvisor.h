2855fb3bd4f5e5f36f08eb11a999501de1ebb147
Lorenzo Susini <susinilorenzo1@gmail.com>
2022-06-08 09:08:46 +0000

    update(gvisor): make runsc root path and trace session config path configurable
    Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

--- userspace/libscap/engine/gvisor/gvisor.h
+++ userspace/libscap/engine/gvisor/gvisor.h
@@ -32,6 +32,8 @@ namespace scap_gvisor {
 constexpr uint32_t min_supported_version = 1;
 constexpr uint32_t current_version = 1;
 constexpr size_t max_line_size = 2048;
+const std::string default_runsc_root_path = "/var/run/docker/runtime-runc/moby";
+const std::string default_trace_session_config_path = "/home/ubuntu/falcosecurity/libs/userspace/libscap/engine/gvisor/config.json";
 
 #pragma pack(push, 1)
 struct header
@@ -93,7 +95,7 @@ class engine {
 public:
     engine(char *lasterr);
     ~engine();
-    int32_t init(std::string socket_path);
+    int32_t init(std::string socket_path, std::string runsc_root_path, std::string trace_session_config_path);
     int32_t close();
 
     int32_t start_capture();
@@ -124,8 +126,8 @@ private:
     // stores per-sandbox data. All buffers used to contain parsed event data are owned by this map
     std::unordered_map<int, sandbox_entry> m_sandbox_data;
 
-    std::string m_root_path = "/var/run/docker/runtime-runc/moby";
-	std::string m_podinit_path = "/home/ubuntu/falcosecurity/libs/userspace/libscap/engine/gvisor/config.json";
+    std::string m_runsc_root_path;
+	std::string m_trace_session_config_path;
 };
 
 } // namespace scap_gvisor
