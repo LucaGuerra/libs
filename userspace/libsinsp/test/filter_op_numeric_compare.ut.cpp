70bf34248feb711185dc3a4143145e2c08870fe8
Luca Guerra <luca@guerra.sh>
2024-06-14 11:57:54 +0000

    test

--- userspace/libsinsp/test/filter_op_numeric_compare.ut.cpp
+++ userspace/libsinsp/test/filter_op_numeric_compare.ut.cpp
@@ -57,3 +57,32 @@ TEST_F(sinsp_with_test_input, signed_int_compare)
 	EXPECT_FALSE(eval_filter(evt, "fd.num < -1"));
 	EXPECT_TRUE(eval_filter(evt, "fd.num > -65535"));
 }
+
+#define eval_and_print(filter) std::cout << filter << " = " << std::boolalpha << eval_filter(evt, filter) << std::endl
+#define print_field(field) std::cout << field << " = " << get_field_as_string(evt, field) << std::endl
+
+TEST_F(sinsp_with_test_input, the_math_isnt_mathing)
+{
+	add_default_init_thread();
+	open_inspector();
+
+	// [PPME_SYSCALL_EPOLL_CREATE_E] = {"epoll_create", EC_WAIT | EC_SYSCALL, EF_CREATES_FD | EF_MODIFIES_STATE, 1, { {"size", PT_INT32, PF_DEC} } },
+	sinsp_evt * evt = add_event_advance_ts(increasing_ts(), 1, PPME_SYSCALL_EPOLL_CREATE_E, 1, (int32_t)-22);
+
+	print_field("evt.rawarg.size"); // 4294967274
+	// The above is cool. Basically the way `rawarg` works is by calling sinsp_utils::find_longest_matching_evt_param("size") which
+	// will return the _first_ parameter that is called "size" in the even table. "size" happens to match 
+	// 	[PPME_SYSCALL_READ_E] = {"read", EC_IO_READ | EC_SYSCALL, EF_USES_FD | EF_READS_FROM_FD, 2, {{"fd", PT_FD, PF_DEC}, {"size", PT_UINT32, PF_DEC} } },
+	// and so it is an uint32, so it is interpreted as unsigned.
+
+	eval_and_print("evt.rawarg.size < 0"); // false , obvious consequence from above
+	eval_and_print("evt.rawarg.size > 10000"); // true , same
+
+	// the above is clearly a bug
+
+	EXPECT_EQ(get_field_as_string(evt, "evt.cpu"), "1"); // 16 bit signed integer
+
+	eval_and_print("evt.cpu > -98305"); // false
+	// the above is simply interpreted by flt_compare_numeric as 16 bit signed integer
+	// so the higher bits are just lost, meaning that -98305 is effectively 32767
+}
